5b27f94538e42d9595f961f04ed528077fcdb4d4
Arun Mathai <me@arunmathaisk.in>
2022-09-07 21:15:55 +0530

    update3

--- absjl/routes/signup.py
+++ absjl/routes/signup.py
@@ -4,7 +4,7 @@ from absjl import app
 import json
 import bcrypt
 import bleach
-
+import re
 
 @app.get('/signup')
 def signup():    
@@ -18,22 +18,27 @@ def signup_post():
     password = data['password']
     firstname = bleach.linkify(bleach.clean(data['firstname']))
     lastname = bleach.linkify(bleach.clean(data['lastname']))
-    
-    email  = Student.query.filter_by(email_id=email_id).first()
-
-    if email is None:
-        bytePwd = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(bytePwd,salt)
-        tmp_student = Student(email_id=email_id,password=hashed,firstname=firstname,lastname=lastname)
-        db.session.add(tmp_student)
-        db.session.commit()
-        res_obj = {}
-        res_obj.update({"status":"OK"})
-        res_obj.update({"message":"Successful !. Please go ahead and login"})
-        return json.dumps(res_obj)
+    pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if re.fullmatch(pat,email_id) and email_id.endswith("@apsit.edu.in"):
+        email  = Student.query.filter_by(email_id=email_id).first()
+        if email is None:
+            bytePwd = password.encode('utf-8')
+            salt = bcrypt.gensalt()
+            hashed = bcrypt.hashpw(bytePwd,salt)
+            tmp_student = Student(email_id=email_id,password=hashed,firstname=firstname,lastname=lastname)
+            db.session.add(tmp_student)
+            db.session.commit()
+            res_obj = {}
+            res_obj.update({"status":"OK"})
+            res_obj.update({"message":"Successful !. Please go ahead and login"})
+            return json.dumps(res_obj)
+        else:
+            res_obj = {}
+            res_obj.update({"status":"ERROR"})
+            res_obj.update({"error":"Email already registered with"})
+            return json.dumps(res_obj)
     else:
         res_obj = {}
         res_obj.update({"status":"ERROR"})
-        res_obj.update({"error":"Email already registered with"})
+        res_obj.update({"error":"Check for valid Email-ID and use Organization/College provided Email-ID"})
         return json.dumps(res_obj)
