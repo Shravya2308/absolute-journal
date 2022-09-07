5794efc4b9ff9edc4b9405132fba74643608204f
Arun Mathai <me@arunmathaisk.in>
2022-09-07 21:00:39 +0530

    update1

--- absjl/modals/dbschema.py
+++ absjl/modals/dbschema.py
@@ -2,11 +2,10 @@ from absjl import db
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False, unique=True)
     email_id = db.Column(db.String(30),  nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False,unique=False)
-    # first_name = db.Column(db.String(15), nullable=True, unique=False)
-    # last_name = db.Column(db.String(15), nullable=True, unique=False)
+    firstname = db.Column(db.String(15), nullable=False, unique=False)
+    lastname = db.Column(db.String(15), nullable=False, unique=False)
     # email_id= db.Column(db.String(15), nullable=False, unique=True)
     # password = db.Column(db.String(300), nullable=False)
     # profile_pic = db.Column(db.String(300), nullable=False)
--- absjl/routes/signup.py
+++ absjl/routes/signup.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, redirect, request, session
+from absjl.modals.dbschema import db, Student
 from absjl import app
+import json
+import bcrypt
+import bleach
+
 
 @app.get('/signup')
 def signup():    
@@ -9,8 +14,35 @@ def signup():
 @app.post('/signup')
 def signup_post():
     data = request.get_json()
-    username = data['username']
     email_id= data['email_id']
     password = data['password']
-    print(username + "    " + email_id + "    " + password)
+    firstname = data['firstname']
+    lastname = data['lastname']
+    
+    email  = Student.query.filter_by(email_id=email_id).first()
+
+    if email is None:
+        bytePwd = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(bytePwd,salt)
+        tmp_student = Student(email_id=email_id,password=hashed,firstname=firstname,lastname=lastname)
+        db.session.add(tmp_student)
+        db.session.commit()
+        res_obj = {}
+        res_obj.update({"status":"OK"})
+        res_obj.update({"message":"Successful !. Please go ahead and login"})
+        return json.dumps(res_obj)
+    else:
+        res_obj = {}
+        res_obj.update({"status":"ERROR"})
+        res_obj.update({"error":"Email already registered with"})
+        return json.dumps(res_obj)
+
+        
+
+       
+
+
+
+
     return data
