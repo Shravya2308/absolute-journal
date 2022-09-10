05aed39c3ec327a1744482cabd81cdfc8e6c873b
Shravya2308 <shravyashetty2308@gmail.com>
2022-09-10 15:27:07 +0530

    efeafrrgerf
    soham karo

--- absjl/static/login.js
+++ absjl/static/login.js
@@ -1,51 +1,93 @@
-document.getElementById("submit").addEventListener("click", function(event){
-    event.preventDefault()
-    var email_id=document.getElementById("email_id").value
-    var password=document.getElementById("password").value
+// document.getElementById("submit1").addEventListener("click", function(event){
+//     event.preventDefault()
+//     var email_id=document.getElementById("email_id1").value
+//     var password=document.getElementById("password1").value
 
-    localStorage.setItem("email_id", email_id);
-    submit(email_id,password)
-  });
-  function submit(email_id,password){
-    // console.log("still yeh")
-    cred={
-      email_id:email_id,
-      password:password
-    }
-    console.log(cred)
-    fetch(window.location.origin+"/login1", {
+//     localStorage.setItem("email_id", email_id);
+//     submit(email_id,password)
+//   });
+//   function submit(email_id,password){
+//     // console.log("still yeh")
+//     cred={
+//       email_id:email_id,
+//       password:password
+//     }
+//     console.log(cred)
+//     fetch(window.location.origin+"/login1", {
        
-      // Adding method type
-      method: "POST",
+//       // Adding method type
+//       method: "POST",
        
-      // Adding body or contents to send
-      body: JSON.stringify(cred),
+//       // Adding body or contents to send
+//       body: JSON.stringify(cred),
        
-      // Adding headers to the request
-      headers: {
-          "Content-type": "application/json; charset=UTF-8"
-      }
-  })
+//       // Adding headers to the request
+//       headers: {
+//           "Content-type": "application/json; charset=UTF-8"
+//       }
+//   })
    
-  // Converting to JSON
-    .then(response => response.json())
+//   // Converting to JSON
+//     .then(response => response.json())
    
-  // Displaying results to console
-    .then(json=>validateLogin(json));
+//   // Displaying results to console
+//     .then(json=>validateLogin(json));
+//   }
+//   function validateLogin(json){
+//     if(json.status){
+//       var status=document.getElementsByClassName("status")
+//       status[0].innerHTML=json.error;
+//     }else{
+//       window.location= window.location.origin+"/home";
+//     }
+//   }
+
+// async function getData(){
+//   const response = await fetch("http://127.0.0.1:5000/get_registered_students");
+//   const data = await response.JSON();
+//   console.log(data);
+
+// }
+// getData()
+document.getElementById("submit1").addEventListener("click", function(event){
+  event.preventDefault()
+  var email_id1=document.getElementById("email_id1").value
+  var password1=document.getElementById("password1").value
+  localStorage.setItem("email_id1", email_id1);
+  submit(email_id1,password1)
+});
+function submit(email_id1,password1){
+  // console.log("still yeh")
+  cred={
+    email_id1:email_id1,
+    password:password1
   }
-  function validateLogin(json){
-    if(json.status){
-      var status=document.getElementsByClassName("status")
-      status[0].innerHTML=json.error;
-    }else{
-      window.location= window.location.origin+"/home";
+  console.log(cred)
+  fetch(window.location.origin+"/login1", {
+     
+    // Adding method type
+    method: "POST",
+     
+    // Adding body or contents to send
+    body: JSON.stringify(cred),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
     }
-  }
-
-async function getData(){
-  const response = await fetch("http://127.0.0.1:5000/get_registered_students");
-  const data = await response.JSON();
-  console.log(data);
-
+})
+ 
+// Converting to JSON
+  .then(response => response.json())
+ 
+// Displaying results to console
+  .then(json=>validateLogin(json));
 }
-getData()
+function validateLogin(json){
+  if(json.status){
+    var status=document.getElementsByClassName("status")
+    status[0].innerHTML=json.error;
+  }else{
+    window.location= window.location.origin+"/home";
+  }
+}
--- absjl/static/signup.js
+++ absjl/static/signup.js
@@ -1,46 +1,46 @@
-document.getElementById("submit").addEventListener("click", function(event){
-    event.preventDefault()
-    var email_id =document.getElementById("email_id").value
-    var password =document.getElementById("password").value
-    var firstname =document.getElementById("firstname").value
-    var lastname =document.getElementById("lastname").value
-    submit(email_id,password,firstname,lastname)
-  });
-function submit(email_id,password,firstname,lastname){
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault()
+  var email_id = document.getElementById("email_id").value
+  var password = document.getElementById("password").value
+  var firstname = document.getElementById("firstname").value
+  var lastname = document.getElementById("lastname").value
+  submit(email_id, password, firstname, lastname)
+});
+function submit(email_id, password, firstname, lastname) {
 
+  cred = {
+    email_id: email_id,
+    password: password,
+    firstname: firstname,
+    lastname: lastname
+  }
+  console.log(cred)
+  fetch(window.location.origin + "/signup", {
 
-    // console.log("still yeh")
-    cred={
-      email_id:email_id,
-      password:password,
-      firstname:firstname,
-      lastname:lastname
+    // Adding method type
+    method: "POST",
+
+    // Adding body or contents to send
+    body: JSON.stringify(cred),
+
+    // Adding headers to the request
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
     }
-    console.log(cred)
-  //   fetch(window.location.origin+"/signup", {
-       
-  //     // Adding method type
-  //     method: "POST",
-       
-  //     // Adding body or contents to send
-  //     body: JSON.stringify(cred),
-       
-  //     // Adding headers to the request
-  //     headers: {
-  //         "Content-type": "application/json; charset=UTF-8"
-  //     }
-  // })
-  //  // Converting to JSON
-  //  .then(response => response.json())
+  })
+    // Converting to JSON
+    .then(response => response.json())
+
+    // Displaying results to console
+    // .then(json => console.log(json))
+    .then(json => validateRegister(json))
+}
+ function validateRegister(json){
    
-  //  // Displaying results to console
-  //    .then(json => validateRegister(json))
+   if(json.message){
+     alert(json.message)
+   }
+   if(json.error){
+    alert(json.error)
+  }
  }
-//  function validateRegister(json){
-//    var status=document.getElementsByClassName("status")
-//    if(json.status){
-//      status[0].innerHTML=json.error;
-//    }else{
-//      status[0].innerHTML=json.message;
-//    }
-//  }
